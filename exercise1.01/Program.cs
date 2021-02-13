a3952e081ba913247cff3aa0cf1f591c0ead53c4
Artur Shomarov <shomarov@protonmail.com>
2021-02-13 19:19:26 +0200

    Exercise 1.01: Getting started

---
+++ exercise1.01/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace exercise1
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var randomString = Guid.NewGuid().ToString();
+
+            while (true)
+            {
+                Console.WriteLine($"{DateTime.UtcNow.ToString("o")} {randomString}");
+                await Task.Delay(5000);
+            }
+        }
+    }
+}
